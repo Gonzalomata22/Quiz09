7d23b8e384afaa43a9a384f9b67fb5f9c17d3033
Gonzalo Mata <gonzalomatah96@gmail.com>
2015-04-28 17:55:27 -0500

    Done! :D

--- Quiz11/Quiz11.cpp
+++ Quiz11/Quiz11.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 void readNumbersFromFile(string random_numbers){
-	float average = 0.0, Total = 0.0, var = 0.0;
+	float average = 0.0, Total = 0.0, var = 0.0, variation = 0.0;
 	int counter=0, count = 0;
 	string line;
+	vector <int> numbers;
 	ifstream theFile(random_numbers);
 	if(theFile.is_open()){
 		while(getline(theFile,line)){
-			int lines = stoi(line);
-			Total = Total + lines;
+			int number = stoi(line);
+			Total = Total + number;
 			counter++;
+			numbers.push_back(number);
 		}
 	}
 
 	average = Total/counter;
-	if(theFile.is_open()){
-		while(getline(theFile, line)){
-			int lines = stoi(line);
-			var = var + (lines - average) * (lines - average);
-		}
+
+	for(int i = 0; i < numbers.size(); i++){
+		var = var + ((numbers[i] - average)*(numbers[i] - average));
 	}
+	variation = var/numbers.size();
 
 	cout<< counter << " lines in that file"<< endl;
 	cout << "The total of the values is: " << Total << endl;
 	cout << "The average of those values is: " << average << endl;
-	cout << "The Standard Deviation in this file is: " << sqrt(var) << endl;
+	cout << "The Standard Deviation in this file is: " << sqrt(variation) << endl;
 }
 
 int main(){
